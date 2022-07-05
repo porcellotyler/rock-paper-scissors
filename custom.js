17606d06c2c925fe4ca002135121aecdc87c88d7
porcellotyler <porcellotyler@gmail.com>
2022-07-04 20:03:21 -0400

    Adjust game from one round to best of five rounds.

--- custom.js
+++ custom.js
@@ -1,55 +1,78 @@
+let globalStart = prompt('Would you like to play?', '');
+let globalStartAnswer = globalStart.charAt(0).toUpperCase() + globalStart.slice(1).toLowerCase();
+let startGame = letsPlay(globalStartAnswer);
 
-/* pseudocode
+var gameResult = 'a string';
 
-create 3 variables for each move in the game
+function letsPlay(answer) {
+    let gameLoop = gamePlay(globalStartAnswer)
 
-computerPlay (randomly pick 1 of 3 variables)
-playerPlay (prompt user to pick a move)
+    function gamePlay(z) {
+        if (z != 0) {
+            var playerScore = 0;
+            var computerScore = 0;
+            for (let i = 0; i < 5; i++) {
+                if (i <= 5) { 
+                    if (answer === 'Yes') {
+                        
+                        let playerPick = prompt('Enter your move', '');
+                        let playerSelection = playerPick.charAt(0).toUpperCase() + playerPick.slice(1).toLowerCase(); /* this takes playPick and formats it to first letter capitalized, rest lowercase so the function below can use it */   
 
-computerSelection (converts computerPlay into RPS move)
-playerSelection (converts playerPlay into RPS move)
+                        let goComputer = generate(playerSelection);
 
-round (compares computerSelection and playSelection)
-    if computer beats player, display "You Lose! Paper beats Rock"
-    else if player beats computer, display "You Win! Paper beats Rock"
-    else display "It's a tie! Both players chose paper"
+                        function generate(x) {
+                            if (x === 'Rock' || x === 'Paper' || x === 'Scissors') {
+                                let moveArray = [ 'Rock', 'Paper', 'Scissors' ];
+                                let computerPlay = moveArray[Math.floor(Math.random()*moveArray.length)];
+                                playRound(computerPlay, playerSelection);
+                            } else { prompt ("You're playing a different game.") }
+                        };
 
-computer displays who won and what player and computer picked
+                        function playRound(a, b) { 
+                            let result;
+                            if (a === 'Rock' && b === 'Scissors') {
+                                result = "You lose the round! Rock beats Scissors";
+                            } else if (a === 'Rock' && b === 'Paper') {
+                                result = "You win the round! Paper beats Rock"; 
+                            } else if (a === 'Scissors' && b === 'Rock') {
+                                result = "You win the round! Rock beats Scissors";
+                            } else if (a === 'Scissors' && b === 'Paper') {
+                                result = "You lose the round! Scissors beats Paper";
+                            } else if (a === 'Paper' && b === 'Scissors') {
+                                result = "You win the round! Scissors beats Paper";
+                            } else if (a === 'Paper' && b === 'Rock') {
+                                result = "You lose the round! Paper beats Rock";
+                            } else {
+                                result = "The round is a tie! Both players chose the same move";
+                            }
+                            console.log(result);
+                            return gameScore(result);
+                        }
 
-*/
+                        function gameScore(y) {
+                            let win = /win/;
+                            let lose = /lose/;
 
-let moveArray = [
-    'Rock',
-    'Paper',
-    'Scissors'
-];
-
-let randomNumber = Math.floor(Math.random()*moveArray.length);
-
-computerPlay = moveArray[randomNumber];
-
-let playerPick = prompt('Enter your move', '');
-
-let playerSelection = playerPick.charAt(0).toUpperCase() + playerPick.slice(1).toLowerCase(); /* this takes playPick and formats it to first letter capitalized, rest lowercase so the function below can use it */
-
-let round = play(computerPlay, playerSelection); 
-
-function play(a, b) {
-    let result;
-    if (a === 'Rock' && b === 'Scissors') {
-       result = console.log("You Lose! Rock beats Scissors");
-    } else if (a === 'Rock' && b === 'Paper') {
-        result = console.log("You Win! Paper beats Rock"); 
-    } else if (a === 'Scissors' && b === 'Rock') {
-        result = console.log("You Win! Rock beats Scissors");
-    } else if (a === 'Scissors' && b === 'Paper') {
-        result = console.log("You Lose! Scissors beats Paper");
-    } else if (a === 'Paper' && b === 'Scissors') {
-        result = console.log("You Win! Scissors beats Paper");
-    } else if (a === 'Paper' && b === 'Rock') {
-        result = console.log("You Lose! Paper beats Rock");
-    } else {
-        result = console.log("It's a tie! Both players chose the same move");
-    }
-    return result;
+                            if (win.test(y) == true) {
+                                ++playerScore; 
+                            } else if (lose.test(y) == true) {
+                                ++computerScore;
+                            }  
+                        }
+                    }
+                } else if (i >= 5) {
+                    break; 
+                }
+            }
+            if (playerScore > computerScore) {
+                gameResult = "You win the game!";
+            } else if (computerScore > playerScore) {
+                gameResult = "You lose the game!"; 
+            } else if (playerScore === computerScore) {
+                gameResult = "The game is a tie! What an even match.";
+            } 
+        
+            console.log(gameResult);            
+        }
+    }    
 };
