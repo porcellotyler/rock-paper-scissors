ba4f794dc6a790046232cff90d7c65fd66e0f56b
porcellotyler <porcellotyler@gmail.com>
2022-07-10 17:46:25 -0400

    Add basic UI, restructuredJS for first to five wins the game

--- custom.js
+++ custom.js
@@ -1,78 +1,81 @@
-let globalStart = prompt('Would you like to play?', '');
-let globalStartAnswer = globalStart.charAt(0).toUpperCase() + globalStart.slice(1).toLowerCase();
-let startGame = letsPlay(globalStartAnswer);
+const btnDiv = document.createElement('div');
+    container.appendChild(btnDiv);
 
-var gameResult = 'a string';
+const rockBtn = document.createElement('button');
+    rockBtn.textContent = 'Rock';
+    btnDiv.appendChild(rockBtn);
+    rockBtn.addEventListener('click', () => generate('Rock'));
+const paperBtn = document.createElement('button');
+    paperBtn.textContent = 'Paper';
+    btnDiv.appendChild(paperBtn);
+    paperBtn.addEventListener('click', () => generate('Paper'));
+const scissorsBtn = document.createElement('button');
+    scissorsBtn.textContent = 'Scissors';
+    btnDiv.appendChild(scissorsBtn);
+    scissorsBtn.addEventListener('click', () => generate('Scissors'));
 
-function letsPlay(answer) {
-    let gameLoop = gamePlay(globalStartAnswer)
+const resultsDiv = document.createElement('div');
+    container.appendChild(resultsDiv);
 
-    function gamePlay(z) {
-        if (z != 0) {
-            var playerScore = 0;
-            var computerScore = 0;
-            for (let i = 0; i < 5; i++) {
-                if (i <= 5) { 
-                    if (answer === 'Yes') {
-                        
-                        let playerPick = prompt('Enter your move', '');
-                        let playerSelection = playerPick.charAt(0).toUpperCase() + playerPick.slice(1).toLowerCase(); /* this takes playPick and formats it to first letter capitalized, rest lowercase so the function below can use it */   
+const scoreDiv = document.createElement('div');
+    container.appendChild(scoreDiv);
 
-                        let goComputer = generate(playerSelection);
+const gameWinner = document.createElement('div');
+    container.appendChild(gameWinner);
 
-                        function generate(x) {
-                            if (x === 'Rock' || x === 'Paper' || x === 'Scissors') {
-                                let moveArray = [ 'Rock', 'Paper', 'Scissors' ];
-                                let computerPlay = moveArray[Math.floor(Math.random()*moveArray.length)];
-                                playRound(computerPlay, playerSelection);
-                            } else { prompt ("You're playing a different game.") }
-                        };
+var playerScore = 0;
+var computerScore = 0;
 
-                        function playRound(a, b) { 
-                            let result;
-                            if (a === 'Rock' && b === 'Scissors') {
-                                result = "You lose the round! Rock beats Scissors";
-                            } else if (a === 'Rock' && b === 'Paper') {
-                                result = "You win the round! Paper beats Rock"; 
-                            } else if (a === 'Scissors' && b === 'Rock') {
-                                result = "You win the round! Rock beats Scissors";
-                            } else if (a === 'Scissors' && b === 'Paper') {
-                                result = "You lose the round! Scissors beats Paper";
-                            } else if (a === 'Paper' && b === 'Scissors') {
-                                result = "You win the round! Scissors beats Paper";
-                            } else if (a === 'Paper' && b === 'Rock') {
-                                result = "You lose the round! Paper beats Rock";
-                            } else {
-                                result = "The round is a tie! Both players chose the same move";
-                            }
-                            console.log(result);
-                            return gameScore(result);
-                        }
+function generate(playersInput) {
+    if (playersInput === 'Rock' || playersInput === 'Paper' || playersInput === 'Scissors') {
+        let moveArray = [ 'Rock', 'Paper', 'Scissors' ];
+        let computerPlay = moveArray[Math.floor(Math.random()*moveArray.length)];
+        playRound(computerPlay, playersInput);
+    };
+};
 
-                        function gameScore(y) {
-                            let win = /win/;
-                            let lose = /lose/;
+function playRound(a, b) { 
+    let result;
+    if (a === 'Rock' && b === 'Scissors') {
+        result = "You lose the round! Rock beats Scissors";
+    } else if (a === 'Rock' && b === 'Paper') {
+        result = "You win the round! Paper beats Rock"; 
+    } else if (a === 'Scissors' && b === 'Rock') {
+        result = "You win the round! Rock beats Scissors";
+    } else if (a === 'Scissors' && b === 'Paper') {
+        result = "You lose the round! Scissors beats Paper";
+    } else if (a === 'Paper' && b === 'Scissors') {
+        result = "You win the round! Scissors beats Paper";
+    } else if (a === 'Paper' && b === 'Rock') {
+        result = "You lose the round! Paper beats Rock";
+    } else {
+        result = "The round is a tie! Both players chose the same move";
+    }
+    resultsDiv.textContent = result;
+    return gameScore(result);
+};
 
-                            if (win.test(y) == true) {
-                                ++playerScore; 
-                            } else if (lose.test(y) == true) {
-                                ++computerScore;
-                            }  
-                        }
-                    }
-                } else if (i >= 5) {
-                    break; 
-                }
-            }
-            if (playerScore > computerScore) {
-                gameResult = "You win the game!";
-            } else if (computerScore > playerScore) {
-                gameResult = "You lose the game!"; 
-            } else if (playerScore === computerScore) {
-                gameResult = "The game is a tie! What an even match.";
-            } 
-        
-            console.log(gameResult);            
-        }
-    }    
+function gameScore(outcome) {
+    let win = /win/;
+    let lose = /lose/;
+
+    if (win.test(outcome) == true) {
+        ++playerScore; 
+    } else if (lose.test(outcome) == true) {
+        ++computerScore;
+    }  
+    scoreDiv.textContent = `Player Score: ${playerScore}, Computer Score: ${computerScore}`
+    return findWinner(playerScore, computerScore);
+};
+
+function findWinner(playerScore, computerScore) {
+    if (playerScore > computerScore && playerScore >= 5) {
+        gameResult = "You win the game!";
+    } else if (computerScore > playerScore && computerScore >= 5) {
+        gameResult = "You lose the game!"; 
+    } else if (playerScore === computerScore && playerScore === 5) {
+        gameResult = "The game is a tie! What an even match.";
+    } 
+    gameWinner.textContent = gameResult;
+    return;
 };
